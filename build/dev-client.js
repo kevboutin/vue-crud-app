4144f4e7c130145e3c24603e5559209ffd0ed513
Kevin Boutin <kevin.boutin@sbdinc.com>
2018-07-27 13:10:08 -0400

    Reformmatting.

--- build/dev-client.js
+++ build/dev-client.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 
 /* eslint-disable */
-require('eventsource-polyfill')
-var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
+require('eventsource-polyfill');
+var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true');
 
 hotClient.subscribe(function (event) {
   if (event.action === 'reload') {
-    window.location.reload()
+    window.location.reload();
   }
-})
+});
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
+const path = require('path');
+const utils = require('./utils');
+const config = require('../config');
+const vueLoaderConfig = require('./vue-loader.conf');
 
 function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+  return path.join(__dirname, '..', dir);
 }
 
 module.exports = {
@@ -74,4 +74,4 @@ module.exports = {
       }
     ]
   }
-}
+};
